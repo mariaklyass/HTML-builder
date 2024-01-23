9f39698aea6b7730157b33a10b32355e327d0376
mariaklyass <klyassmaria@gmail.com>
2024-01-23 16:35:32 +0300

    feat: solve 1-6 steps in 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,4 +1,4 @@
-//1. Import all required modules (and define paths)
+//1, 2. Import all required modules (and define paths)
 const fs = require('fs');
 const path = require('path');
 //where to put bundle
@@ -14,7 +14,7 @@ if (!fs.existsSync(projectDistPath)) {
   fs.mkdirSync(projectDistPath);
 }
 
-// 2. Find all tag names in the template file
+// 3. Find all tag names in the template file
 let templateContent = fs.readFileSync(templatePath, 'utf-8');
 const tagNames = templateContent
   .match(/{{(.*?)}}/g)
@@ -24,7 +24,7 @@ const matchingFiles = tagNames.map((tag) => {
   const filePath = path.join(componentsPath, `${tag}.html`);
   return fs.existsSync(filePath) ? filePath : null;
 });
-// 3. Replace template tags with the contents of component files
+// 4. Replace template tags with the contents of component files
 matchingFiles.forEach((filePath, index) => {
   if (filePath) {
     const componentContent = fs.readFileSync(filePath, 'utf-8');
@@ -32,10 +32,34 @@ matchingFiles.forEach((filePath, index) => {
     templateContent = templateContent.replace(tagRegex, componentContent);
   }
 });
-// 4. Write the modified template to the index.html file in the project-dist folder
+// 5. Write the modified template to the index.html file in the project-dist folder
 fs.writeFileSync(finalHTML, templateContent);
 
 //6. Use the existing script from task 05-merge-styles to create the style.css file
+const stylesFolderPath = path.join(__dirname, 'styles');
+const finalStylesPath = path.join(__dirname, 'project-dist', 'style.css');
+
+fs.readdir(stylesFolderPath, (err, files) => {
+  if (err) {
+    console.error('Error reading styles folder:', err);
+    return;
+  }
+
+  const cssFiles = files.filter(
+    (file) => path.extname(file).toLowerCase() === '.css',
+  );
+
+  const stylesArray = [];
+  cssFiles.forEach((cssFile) => {
+    const filePath = path.join(stylesFolderPath, cssFile);
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+    stylesArray.push(fileContent);
+  });
+
+  const bundleContent = stylesArray.join('\n');
+  fs.writeFileSync(finalStylesPath, bundleContent, 'utf-8');
+  console.log('Style bundle created successfully:', finalStylesPath);
+});
 
 // 7. Use the script from task 04-copy-directory to move the assets folder into the project-dist folder
 
