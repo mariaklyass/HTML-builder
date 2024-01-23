2a23de07f7a0e938495a4a69f1ce05164e23b0f4
mariaklyass <klyassmaria@gmail.com>
2024-01-23 16:21:54 +0300

    feat: solve 1-4 steps in 06-build-step

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,28 +1,42 @@
-//1. Import all required modules.
+//1. Import all required modules (and define paths)
 const fs = require('fs');
 const path = require('path');
+//where to put bundle
+const projectDistPath = path.join(__dirname, 'project-dist');
+//where to put index.html
+const finalHTML = path.join(__dirname, 'project-dist', 'index.html');
+//where is the template
+const templatePath = path.join(__dirname, 'template.html');
+//where are all html contents
+const componentsPath = path.join(__dirname, 'components');
+
+if (!fs.existsSync(projectDistPath)) {
+  fs.mkdirSync(projectDistPath);
+}
 
 // 2. Find all tag names in the template file
-//?
-
+let templateContent = fs.readFileSync(templatePath, 'utf-8');
+const tagNames = templateContent
+  .match(/{{(.*?)}}/g)
+  .map((tag) => tag.replace(/[{}]/g, '').trim());
+//find matching files
+const matchingFiles = tagNames.map((tag) => {
+  const filePath = path.join(componentsPath, `${tag}.html`);
+  return fs.existsSync(filePath) ? filePath : null;
+});
 // 3. Replace template tags with the contents of component files
-//?
-
+matchingFiles.forEach((filePath, index) => {
+  if (filePath) {
+    const componentContent = fs.readFileSync(filePath, 'utf-8');
+    const tagRegex = new RegExp(`{{${tagNames[index]}}}`, 'g');
+    templateContent = templateContent.replace(tagRegex, componentContent);
+  }
+});
 // 4. Write the modified template to the index.html file in the project-dist folder
-const projectDistPath = path.join(__dirname, 'project-dist');
-const indexPath = path.join(projectDistPath, 'index.html');
-fs.mkdirSync(projectDistPath, { recursive: true });
-
-//  5. Write the modified template to the `index.html` file in the `project-dist` folder.
+fs.writeFileSync(finalHTML, templateContent);
 
 //6. Use the existing script from task 05-merge-styles to create the style.css file
-const stylesFolderPath = path.join(__dirname, 'styles');
-const stylePath = path.join(projectDistPath, 'style.css');
 
 // 7. Use the script from task 04-copy-directory to move the assets folder into the project-dist folder
-const copyDirectory = require('../04-copy-directory');
-const assetsSrc = path.join(__dirname, 'assets');
-const assetsDest = path.join(projectDistPath, 'assets');
-copyDirectory(assetsSrc, assetsDest);
 
 //*to be done*
