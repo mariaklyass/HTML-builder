45b8152f9f731db5031eb817c2d2b1c6f02c097f
mariaklyass <klyassmaria@gmail.com>
2024-01-24 21:30:27 +0300

    fix: adjust 05-merge-styles to the task's rules

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,29 +1,41 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const stylesFolderPath = path.join(__dirname, 'styles');
 const outputFolderPath = path.join(__dirname, 'project-dist');
 const outputFile = path.join(outputFolderPath, 'bundle.css');
 
-fs.readdir(stylesFolderPath, (err, files) => {
-  if (err) {
-    console.error('Error reading styles folder:', err);
-    return;
-  }
-  const cssFiles = files.filter(
-    (file) => path.extname(file).toLowerCase() === '.css',
-  );
-  const stylesArray = [];
-  cssFiles.forEach((cssFile) => {
-    const filePath = path.join(stylesFolderPath, cssFile);
-    const fileContent = fs.readFileSync(filePath, 'utf-8');
-    stylesArray.push(fileContent);
-  });
-  const bundleContent = stylesArray.join('\n');
-  if (!fs.existsSync(outputFolderPath)) {
-    fs.mkdirSync(outputFolderPath);
-  }
+async function bundleStyles() {
+  try {
+    const files = await fs.readdir(stylesFolderPath);
+    const cssFiles = files.filter(
+      (file) => path.extname(file).toLowerCase() === '.css',
+    );
+    const stylesArray = await Promise.all(
+      cssFiles.map(async (cssFile) => {
+        const filePath = path.join(stylesFolderPath, cssFile);
+        const fileContent = await fs.readFile(filePath, 'utf-8');
+        return fileContent;
+      }),
+    );
 
-  fs.writeFileSync(outputFile, bundleContent, 'utf-8');
-  console.log('Bundle created successfully:', outputFile);
-});
+    const bundleContent = stylesArray.join('\n');
+
+    try {
+      await fs.mkdir(outputFolderPath);
+    } catch (mkdirErr) {
+      if (mkdirErr.code !== 'EEXIST') {
+        console.error(
+          `Error creating directory ${outputFolderPath}:`,
+          mkdirErr,
+        );
+        throw mkdirErr;
+      }
+    }
+
+    await fs.writeFile(outputFile, bundleContent, 'utf-8');
+  } catch (err) {
+    console.error('Error reading or writing files: ', err);
+  }
+}
+bundleStyles();
